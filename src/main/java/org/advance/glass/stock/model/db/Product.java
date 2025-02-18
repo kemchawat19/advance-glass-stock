f93c0c8b09d305f42ae59b09e077c874bc7fa2c0
kemchawat19 <96466021+kemchawat19@users.noreply.github.com>
2025-02-18 22:31:04 +0700

    add product master file

--- src/main/java/org/advance/glass/stock/model/db/Product.java
+++ src/main/java/org/advance/glass/stock/model/db/Product.java
@@ -2,6 +2,7 @@ package org.advance.glass.stock.model.db;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,24 +13,34 @@ import org.hibernate.annotations.UpdateTimestamp;
 import java.time.LocalDateTime;
 
 @Entity
-@Table(name = "stock")
+@Table(name = "product")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-public class Stock {
+public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "product_id", nullable = false)
-    private Product product;
+    @NotNull
+    @Size(max = 4)
+    @Column(name = "product_code", nullable = false, unique = true)
+    private String productCode;
 
     @NotNull
-    @Column(name = "quantity", nullable = false)
-    private int quantity;
+    @Size(max = 255)
+    @Column(name = "product_name", nullable = false)
+    private String productName;
+
+    @Size(max = 100)
+    @Column(name = "product_group")
+    private String productGroup;
+
+    @Size(max = 50)
+    @Column(name = "product_unit")
+    private String productUnit;
 
     @CreationTimestamp
     @Column(updatable = false)
--- src/main/java/org/advance/glass/stock/repository/StockRepository.java
+++ src/main/java/org/advance/glass/stock/repository/StockRepository.java
@@ -3,10 +3,16 @@ package org.advance.glass.stock.repository;
 
 import org.advance.glass.stock.model.db.Stock;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Long> {
 
+    @Query("""
+            SELECT stock
+            FROM Stock stock
+            WHERE stock.product.productCode = :productCode
+            """)
     Stock findByProductCode(String productCode);
 }
